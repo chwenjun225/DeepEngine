49f5bda7737b5933a9f4bf902fa95bfc8ef00aae
chwenjun225 <trantuan22052k@gmail.com>
2025-01-21 08:46:34 +0700

    Update .gitignore to include new directories; refine documentation in main.py and add README files for new components

--- main.py
+++ main.py
@@ -22,15 +22,15 @@ def chat_completion_inference_with_llama3(
 	max_gen_len: Optional[int] = None,
 ):
 	"""
-	Examples to run with the models finetuned for chat. Prompts correspond of chat
-	turns between the user and assistant with the final one always being the user.
+		Examples to run with the models finetuned for chat. Prompts correspond of chat
+		turns between the user and assistant with the final one always being the user.
 
-	An optional system prompt at the beginning to control how the model should respond
-	is also supported.
+		An optional system prompt at the beginning to control how the model should respond
+		is also supported.
 
-	The context window of llama3 models is 8192 tokens, so `max_seq_len` needs to be <= 8192.
+		The context window of llama3 models is 8192 tokens, so `max_seq_len` needs to be <= 8192.
 
-	`max_gen_len` is optional because finetuned models are able to stop generations naturally.
+		`max_gen_len` is optional because finetuned models are able to stop generations naturally.
 	"""
 	generator = Llama.build(
 		ckpt_dir=ckpt_dir,

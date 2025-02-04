499e2d6fc105bfd01faa0c4821e08f19f1b22c55
chwenjun225 <trantuan22052k@gmail.com>
2025-02-04 20:29:18 +0700

    Update benchmark server

---
+++ evals/benchmark_inference_llamacpp_server.md
@@ -0,0 +1,2 @@
+llama.cpp load model trên server chiếm 2331/24564
+vllm load model trên server chiếm 16158/24564
--- utils/utils.sh
+++ utils/utils.sh
@@ -28,15 +28,43 @@ vllm serve /home/chwenjun225/Projects/Foxer/notebooks/DeepSeek-R1-Distill-Qwen-1
 --enable-auto-tool-choice \
 --tool-call-parser llama3_json \
 --max-prompt-adapter-token 2048
+# Benchmark inference VLLM runserver
+python ./third_3rdparty/vllm-0.7.1/benchmarks/benchmark_serving.py \
+--backend openai \
+--base-url http://127.0.0.1:2025 \
+--dataset-name random \
+--model /home/chwenjun225/Projects/Foxer/notebooks/DeepSeek-R1-Distill-Qwen-1.5B_finetune_CoT_ReAct/1_finetuned_DeepSeek-R1-Distill-Qwen-1.5B_finetune_CoT_ReAct \
+--seed 12345 \
+--tokenizer-mode auto \
+--trust-remote-code \
+--use-beam-search 
 
 # llama_cpp runserver
-./third_3rdparty/llama.cpp-b4621/build/bin/llama-server -m /home/chwenjun225/Projects/Foxer/models/DeepSeek-R1-Distill-Qwen-1.5B/gguf/DeepSeek-R1-Distill-Qwen-1.5B-F32.gguf \
+./third_3rdparty/llama.cpp-b4621/build/bin/llama-server -m /home/chwenjun225/Projects/Foxer/notebooks/DeepSeek-R1-Distill-Qwen-1.5B_finetune_CoT_ReAct/1_finetuned_DeepSeek-R1-Distill-Qwen-1.5B_finetune_CoT_ReAct/gguf/1_finetuned_DeepSeek-R1-Distill-Qwen-1.5B_finetune_CoT_ReAct-1.8B-1_finetuned_DeepSeek-R1-Distill-Qwen-1.5B_finetune_CoT_ReAct-F32.gguf \
 --port 2026 
+# 1️. Kiểm tra hiệu suất inference trên llama.cpp
+./third_3rdparty/llama.cpp-b4621/build/bin/main -m models/your_model.gguf -p "A machine reports high vibration levels. What should be done?" \
+--repeat_prompt \
+--threads 8 \
+--n 256
+# -m models/your_model.gguf → Load mô hình .gguf.
+# -p → Dùng prompt để kiểm tra inference.
+# --repeat_prompt → Lặp lại lệnh để đo tốc độ.
+# --threads 8 → Sử dụng 8 luồng CPU (Tăng hoặc giảm tùy phần cứng).
+# --n 256 → Sinh ra 256 token đầu ra, giúp đo tốc độ token/s.
+# 2. Chạy benchmark tốc độ inference
+./third_3rdparty/llama.cpp-b4621/build/bin/main -m models/your_model.gguf -p "Benchmarking Llama.cpp server" \
+--tokens 256 \
+--threads 8
+
 
 # Convert DeepSeek-R1-Distill-Qwen-1.5B hf to gguf
-python convert_hf_to_gguf.py /home/kali/Projects/Foxer/models/DeepSeek-R1-Distill-Qwen-1.5B \
+python convert_hf_to_gguf.py /home/chwenjun225/Projects/Foxer/notebooks/DeepSeek-R1-Distill-Qwen-1.5B_finetune_CoT_ReAct/1_finetuned_DeepSeek-R1-Distill-Qwen-1.5B_finetune_CoT_ReAct \
 --outtype f32 \
---outfile /home/chwenjun225/Projects/Foxer/models/DeepSeek-R1-Distill-Qwen-1.5B/gguf
+--outfile /home/chwenjun225/Projects/Foxer/notebooks/DeepSeek-R1-Distill-Qwen-1.5B_finetune_CoT_ReAct/1_finetuned_DeepSeek-R1-Distill-Qwen-1.5B_finetune_CoT_ReAct/gguf
 
 # Kill the loaded model VRAM 
 nvidia-smi | grep 'python' | awk '{ print $5 }' | xargs -n1 kill -9
+
+# CUDA static realtime
+watch -n0.3 gpustat -cp --color

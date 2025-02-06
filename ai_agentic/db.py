02b4e86b7946f5cbaa232a952aa8a54d3d37d9f4
chwenjun225 <trantuan22052k@gmail.com>
2025-02-06 20:29:24 +0700

    Update chroma server api

--- ai_agentic/db.py
+++ ai_agentic/db.py
@@ -6,7 +6,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader 
 
 def show_collections(host="127.0.0.1", port=2027):
-	"""Hiển thị danh sách collections hiện có trong server."""
+	"""Hiển thị danh sách collections trong ChromaDB."""
 	client = chromadb.HttpClient(host="127.0.0.1", port=2027)
 	print("List of collections:")
 	for collection in client.list_collections():
@@ -22,7 +22,7 @@ def upload_data_to_server(
 		file_path="/home/chwenjun225/Projects/Foxer/datasets/state_of_the_union.txt", 
 		persist_directory="/home/chwenjun225/Projects/Foxer/ai_agentic/chroma_db"
 	):
-	"""Tải dữ liệu lên chroma_db."""
+	"""Lưu dữ liệu & vector embeddings vào ChromaDB."""
 	client = chromadb.HttpClient(host=host, port=port)
 	collection = client.get_or_create_collection(name=name_of_collection)
 	loader = TextLoader(file_path)
@@ -40,13 +40,22 @@ def upload_data_to_server(
 	)
 	ids = [f"doc_{i}" for i in range(len(split_docs))]
 	documents = [doc.page_content for doc in split_docs]
+	metadatas = [{"source": file_path, "chunk_id": i} for i in range(len(split_docs))]
 	collection.add(
 		ids=ids, 
 		documents=documents, 
-		metadatas=[{"source": file_path} for _ in split_docs], 
+		metadatas=metadatas, 
 	)
-	print(f">>> Đã tải {len(split_docs)} đoạn văn bản vào collection_name: {name_of_collection}")
+	print(f">>> Successfully loaded {len(split_docs)} data & vector to collection: {name_of_collection}")
+
+def query_collection(
+		port=2027, 
+		host="127.0.0.1"
+	):
+	# TODO: Mai hoàn thành module RAG và các api để tương tác với dữ liệu server.
+	pass
 
 if __name__ == "__main__":
 	upload_data_to_server()
 	show_collections()
+	query_collection()

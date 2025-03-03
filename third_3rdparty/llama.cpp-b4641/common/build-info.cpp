7326699473a4332231206acf72b1637373f585b1
chwenjun225 <trantuan22052k@gmail.com>
2025-03-03 08:04:45 +0700

    Update

---
+++ third_3rdparty/llama.cpp-b4641/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 146;
+char const *LLAMA_COMMIT = "ff88a3b";
+char const *LLAMA_COMPILER = "cc (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0";
+char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
